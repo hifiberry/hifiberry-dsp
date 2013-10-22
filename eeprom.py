7f60f12e8f69900fef1474b9937c1c9742d86c23
Daniel Matuschek <daniel@matuschek.net>
2013-10-22 19:37:02 +0200

    Created EEPROM helper module

---
+++ eeprom.py
@@ -0,0 +1,23 @@
+# Simple I2C EEPROM routines
+# works with 16-bit addressable EEPROMs (up to 64kB)
+
+from smbus import SMBus
+
+smb=SMBus(1);
+slaveaddr=0x50;
+
+def eeprom_set_current_address(addr):
+	a1=addr/256;
+	a0=addr%256;
+	smb.write_i2c_block_data(slaveaddr,a1,[a0]);
+
+def eeprom_write_block(addr,data):
+	a1=addr/256;
+	a0=addr%256;
+
+	data.insert(0,a0);
+	smb.write_i2c_block_data(slaveaddr,a1,data);
+
+def eeprom_read_byte(addr):
+	eeprom_set_current_address(addr);
+	return smb.read_byte(slaveaddr);
