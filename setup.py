694118d8598ab286e72151c4fd90fdab3d0562f4
Daniel Matuschek <daniel@matuschek.net>
2018-07-07 16:44:46 +0200

    Added Changelog and TODO version 0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='hifiberrydsp',
-      version='0.4',
+      version='0.5',
       description='A DSP Toolkit',
       long_description=long_description,
       url='http://github.com/hifiberry/hifiberry-dsp',
