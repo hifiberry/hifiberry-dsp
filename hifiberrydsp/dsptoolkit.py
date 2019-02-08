3d47029bc05d25ea462614c8c2fca74c6d1f73a6
Daniel Matuschek <info@hifiberry.com>
2019-02-08 12:00:12 +0100

    Added support for more REW filters

--- hifiberrydsp/dsptoolkit.py
+++ hifiberrydsp/dsptoolkit.py
@@ -85,6 +85,7 @@ class REW():
                 if line.startswith("Filter"):
                     parts = line.split()
                     if len(parts) >= 12 and parts[2] == "ON" and \
+                            parts[3] == "PK" and \
                             parts[4] == "Fc" and parts[6] == "Hz" and \
                             parts[7] == "Gain" and parts[9] == "dB" and \
                             parts[10] == "Q":
@@ -92,10 +93,77 @@ class REW():
                         fc = float(parts[5])
                         gain = float(parts[8])
                         q = float(parts[11])
-                        logging.info("Filter fc=%s, q=%s, gaion=%s, fs=%s",
+                        logging.info("Filter EQ fc=%s, q=%s, gaion=%s, fs=%s",
                                      fc, q, gain, fs)
                         filters.append(
                             Biquad.peaking_eq(fc, q, gain, fs))
+                    elif len(parts) >= 6 and parts[2] == "ON" and \
+                            parts[3] == "LP" and \
+                            parts[4] == "Fc" and parts[6] == "Hz":
+
+                        fc = float(parts[5])
+                        logging.info("Filter LP fc=%s", fc)
+                        filters.append(
+                            Biquad.low_pass(fc, 0.707, fs))
+                    elif len(parts) >= 9 and parts[2] == "ON" and \
+                            parts[3] == "LPQ" and \
+                            parts[4] == "Fc" and parts[6] == "Hz" and \
+                            parts[7] == "Q":
+                        fc = float(parts[5])
+                        q = float(parts[8])
+                        logging.info("Filter LPQ fc=%s, q=%s", fc, q)
+                        filters.append(
+                            Biquad.low_pass(fc, q, fs))
+                    elif len(parts) >= 10 and parts[2] == "ON" and \
+                            parts[3] == "LS" and \
+                            parts[4] == "Fc" and parts[6] == "Hz" and \
+                            parts[7] == "Gain" and parts[9] == "dB":
+                        fc = float(parts[5])
+                        db = float(parts[8])
+                        q = 0.707
+                        logging.info("Filter LS fc=%s, db=%s", fc, db)
+                        filters.append(
+                            Biquad.low_shelf(fc, q, db, fs))
+                    elif len(parts) >= 6 and parts[2] == "ON" and \
+                            parts[3] == "HP" and \
+                            parts[4] == "Fc" and parts[6] == "Hz":
+
+                        fc = float(parts[5])
+                        logging.info("Filter HP fc=%s", fc)
+                        filters.append(
+                            Biquad.high_pass(fc, 0.707, fs))
+                    elif len(parts) >= 9 and parts[2] == "ON" and \
+                            parts[3] == "HPQ" and \
+                            parts[4] == "Fc" and parts[6] == "Hz" and \
+                            parts[7] == "Q":
+                        fc = float(parts[5])
+                        q = float(parts[8])
+                        logging.info("Filter HPQ fc=%s, q=%s", fc, q)
+                        filters.append(
+                            Biquad.high_pass(fc, q, fs))
+                    elif len(parts) >= 10 and parts[2] == "ON" and \
+                            parts[3] == "HS" and \
+                            parts[4] == "Fc" and parts[6] == "Hz" and \
+                            parts[7] == "Gain" and parts[9] == "dB":
+                        fc = float(parts[5])
+                        db = float(parts[8])
+                        q = 0.707
+                        logging.info("Filter HS fc=%s, db=%s", fc, db)
+                        filters.append(
+                            Biquad.high_shelf(fc, q, db, fs))
+                    elif len(parts) >= 7 and parts[2] == "ON" and \
+                            parts[3] == "NO" and \
+                            parts[4] == "Fc" and parts[6] == "Hz":
+                        fc = float(parts[5])
+                        q = 0.707
+                        logging.info("Filter NO fc=%s", fc, db)
+                        filters.append(
+                            Biquad.notch(fc, q, fs))
+
+                    else:
+                        if len(parts) >= 4 and parts[3] != "None":
+                            print("Filter type " + parts[3] +
+                                  " not yet supported")
 
             return filters
 
